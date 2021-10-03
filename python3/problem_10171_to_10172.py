205ba03fee7c7ea8b19c783587496b34c4bf14f5
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2021-10-03 19:36:34 +0900

    bakejoon problem no.10171 and no.10172

---
+++ python3/problem_10171_to_10172.py
@@ -0,0 +1,14 @@
+##problem no.10171
+##https://www.acmicpc.net/problem/10171
+print("\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+##problem no.10172
+##https://www.acmicpc.net/problem/10172
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
