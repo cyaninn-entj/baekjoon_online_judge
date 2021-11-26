fede6989d3395e39e03f3322b2f33dd92a7acd89
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2021-11-26 17:34:36 +0900

    baekjoon online judge problem no.4673
    https://www.acmicpc.net/
    step5-2 funtion

--- python3/step6/4673.py
+++ python3/step6/4673.py
@@ -1,23 +1,20 @@
 #https://www.acmicpc.net/problem/4673
-def selfnumber(number) :
-    selfN=0
-    check=0
-    for thousand in range(0,10) :
-        for hundread in range(0,10) :
-            for ten in range(0,10) :
-                for one in range(0,10) :
-                    if number==(1001*thousand)+(101*hundread)+(11*ten)+(2*one) :
-                        check=check+1
-                    else :
-                        check=check+0
-    if check==0 :
-        selfN=number
-    return selfN
+generated_num=[]
 
-for i in range(1, 10001) :
-    result=selfnumber(i)
-    if result!=0 :
-        print(result)\
+def check_selfnum(number) :
+    if number not in generated_num :
+        return True
+    else :
+         return False
+    
+for i in range(1,10001) :
+    for j in str(i) :
+        i += int(j)
+    generated_num.append(i)
+
+for i in range(1,10001) :
+    if check_selfnum(i)==True :
+        print(i)
 
 
 
