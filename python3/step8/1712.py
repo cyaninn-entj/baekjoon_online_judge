5ca4645859fa1d1ea36dc7baf0bef7a6c013a0ce
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2022-01-19 15:49:53 +0900

    baekjoon online judge problem no.1712
    https://www.acmicpc.net/
    step8- string lv.1

--- python3/step8/1712.py
+++ python3/step8/1712.py
@@ -1,23 +1,18 @@
 #https://www.acmicpc.net/problem/1712
 
+'''
+A+BX < CX
+A < (C-B)X
+A/(C-B) < X
+'''
 A,B,C=input().split()
+A=int(A)
+B=int(B)
+C=int(C)
 
-perA=1
-perB=int(B)/int(A)
-perC=int(C)/int(A)
-
-X=1
-income=perC-perB
-outlay=0
-
-while True :
-    if perB>=perC :
-        X= -1
-        break;
-    outlay=perA/X
-    #print(X, outlay, income)
-    if outlay < income :
-        break;
-    X+=1
-
-print(X)
+if B >= C :
+    X=-1
+else : 
+    X=A/(C-B)+1
+    
+print(int(X))
