5117383ea4a8b6bfbcfa6dbe967becdd9385d198
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2022-02-04 11:44:00 +0900

    baekjoon online judge problem no.1929
    https://www.acmicpc.net/
    step9- basic math2 lv.4

--- python3/step9/1929.py
+++ python3/step9/1929.py
@@ -1,19 +1,24 @@
 #https://www.acmicpc.net/problem/1929
 
 def pn(n,m) :
-    if n==1 : start=2
-    else : start=n
+    start=n
     end=m
-    for i in range(start,end) :
-        cks=1
-        for j in range(2,i) :
-            if j*j<=end :
-                if i%j==0 : cks=0
-                else : pass
-        if cks==1 : print(i)
-    return 0
+
+    a = [False,False] + [True]*(end-1)
+    #print(a)
+    primes=[]
+    for i in range(2,end+1):
+      if a[i]:
+        if i>=start :
+            primes.append(i)
+        for j in range(2*i, end+1, i):
+            a[j] = False
+
+    return primes
                 
     
 _n, _m=map(int, input().split())
 pn_ls=pn(_n,_m)
-#print(pn_ls)
+
+for i in pn_ls :
+    print(i)
